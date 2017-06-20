cee84f0269e4cedbc9b0d82662ebb33b230f1474
Adam Trimble <adam.trimble@intel.com>
2017-06-20 12:41:17 -0700

    import the question data

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
 import React, { Component } from 'react';
-import Question from './question.js';
+import Question from './Question.js';
+import questionData from './multipleQuestions.json';
 import logo from './logo.svg';
 import './App.css';
 
-
-const questionData = {
-  question: 'What is the capital of France?',
-  picture: 'https://en.parisinfo.com/var/otcp/sites/images/node_43/node_51/node_233/seine-nuit-%7C-740x380-%7C-%C2%A9-thinkstock/12354-3-fre-FR/Seine-nuit-%7C-740x380-%7C-%C2%A9-Thinkstock.jpg',
-  answers: [
-    {
-      option: 'London',
-      correct: false
-    },
-    {
-      option: 'Paris',
-      correct: true
-    },
-    {
-      option: 'Rome',
-      correct: false
-    },
-    {
-      option: 'Berlin',
-      correct: false
-    }
-  ]
-};
-
 class App extends Component {
+  constructor() {
+    super();
+    // check your browser console to see what's in questionData
+    // Once you don't need this anymore delete the constructor
+    console.log(questionData);
+  }
   render() {
     return (
       <div className="App">
@@ -36,7 +19,10 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">This is my test space.</p>
-        <Question question={questionData.question} picture={questionData.picture} answers={questionData.answers} />
+        {/* 
+          Delete this comment and map questionData to Question components
+          <Question question={questionData.question} picture={questionData.picture} answers={questionData.answers} />
+        */}
       </div>
     );
   }
