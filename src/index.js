0c319854909f2481002810f2dcc1af92132b5be2
Tini Trimble <tini.trimble@gmail.com>
2017-08-01 16:56:23 -0700

    random css changes and stuff

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Poiret One', 'cursive']
+  }
+})
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
